57340aa6358ce0d87d4ee10d49d402ff8c0cfcfa
rutam-varda <beridze186@gmail.com>
2022-09-28 01:31:18 +0300

    list

--- homework_6/index.php
+++ homework_6/index.php
@@ -1,2 +1,40 @@
 <?php
-$test = 'Hallo';
+
+$arr = [
+    ['name' => 'John', 'email' => 'john@gmail.com', 'tel' => '+380951112233', 'sex' => 'man'],
+    ['name' => 'Kate', 'email' => 'Kate@gmail.com', 'tel' => '+380951112244', 'sex' => 'woman'],
+    ['name' => 'Peter', 'email' => 'Peter@gmail.com', 'tel' => '+380951112255', 'sex' => 'man'],
+    ['name' => 'Jasmin', 'email' => 'Jasmin@gmail.com', 'tel' => '+380951112266', 'sex' => 'woman']
+];
+
+function listArr(array $arr) : string
+{
+    $html = '<ul>';
+    $html .= '<li>';
+    $arrKeys = array_keys($arr[0]);
+    foreach ($arrKeys as $key) {
+        $html .= "<h3>$key</h3>";
+    }
+    $html .= '</li>';
+
+    foreach ($arr as $subArray) {
+        $html .= '<li>';
+        foreach ($subArray as $value) {
+            $html .= "<li>$value</li>";
+        }
+        $html .= '</li>';
+    }
+
+    $html .= '</ul>';
+
+    return $html;
+}
+print_r(listArr($arr));
+?>
+
+<style>
+    ul {
+        list-style: none;
+    }
+</style>
+
